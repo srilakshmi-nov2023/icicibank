68a19fd86fd35fe80e40b40f3baa3f100750aa70
srilakshmi-2425 <srilakshmikintali2015@gmail.com>
2023-12-08 15:09:52 +0530

    update

--- DBUtils.java
+++ DBUtils.java
@@ -1,5 +1,16 @@
 Hello This file contains the DB related logic..
 i am modified this file for practising purpose
 i am update this file staging branch
+<<<<<<< HEAD
 hi today i am created one more branch called uat
 and i will save in uat branch
+=======
+<<<<<<< HEAD
+
+today i am  created a branch name is 'uat' 
+and updated the file in uat
+=======
+hi today i am created one more branch called uat
+and i will save in uat branch
+>>>>>>> f770886 (updated in uat)
+>>>>>>> 6702a60 (modified)
